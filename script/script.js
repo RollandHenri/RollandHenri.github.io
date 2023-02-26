ff268bcef2e579981567505b485b83c9664c5e96
RollandHenri <rolland.h.dev@gmail.com>
2023-02-26 23:43:22 +0100

    folio
    day3 progress JS

--- script/script.js
+++ script/script.js
@@ -12,8 +12,11 @@ const navService = document.querySelector(".navService");
 window.addEventListener("scroll", () => {
   if (window.scrollY > 30) {
     navbar.style.height = "40px";
+    navbar.style.background = "grey";
+    navbar.style.zIndex = 8;
   } else {
     navbar.style.height = "80px";
+    navbar.style.background = "rgba(179, 179, 179, 0.74)";
   }
 });
 
@@ -22,31 +25,31 @@ window.addEventListener("scroll", () => {
 window.addEventListener("scroll", () => {
   let scrollratio = (window.scrollY + innerHeight) / document.body.offsetHeight;
 
-  if (scrollratio < 0.5) {
+  if (scrollratio < 0.75) {
     navAbout.style.color = "black";
     navAbout.style.textShadow = "none";
   }
-  if (scrollratio > 0.5) {
+  if (scrollratio > 0.75) {
     navAbout.style.textShadow = "1px 5px 5px white";
     navAbout.style.color = "rgb(48, 48, 88)";
   }
-  if (scrollratio > 0.87) {
+  if (scrollratio > 1.2) {
     navAbout.style.textShadow = "none";
     navAbout.style.color = "black";
     navSkill.style.textShadow = "1px 5px 5px white";
     navSkill.style.color = "rgb(48, 48, 88)";
   }
-  if (scrollratio < 0.87) {
+  if (scrollratio < 1.2) {
     navSkill.style.textShadow = "none";
     navSkill.style.color = "black";
   }
-  if (scrollratio > 1.35) {
+  if (scrollratio > 1.7) {
     navService.style.textShadow = "1px 5px 5px white";
     navService.style.color = "rgb(48, 48, 88)";
     navSkill.style.textShadow = "none";
     navSkill.style.color = "black";
   }
-  if (scrollratio < 1.35) {
+  if (scrollratio < 1.7) {
     navService.style.textShadow = "none";
     navService.style.color = "black";
   }
